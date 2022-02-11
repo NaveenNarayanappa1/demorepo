e2c2f4c2ce2b94e44834b6bb465b9cdb38b40849
naveennarayanappa1 <nnaveen840@gmail.com>
2022-02-11 12:49:53 +0530

    this is my first commit from intellj file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-//this is the main method
+//this is the main method naveen
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
